d5f547a7505c5cead35819a152f7536af14d63df
DariuszDabrowski <darkos81@wp.pl>
2017-08-22 21:25:14 +0200

    sierpien,22 cd

--- src/Shape/Square.java
+++ src/Shape/Square.java
@@ -1,9 +1,35 @@
 package Shape;
 
-/**
- * Created by RENT on 2017-08-22.
- */
 public class Square extends Rectangle{
+
+    public Square() {}
+
+    public Square(double side) {
+        super.setWith(side);
+        super.setLength(side);
+    }
+    public Square(double side, String color, Boolean filled) {
+        super.setWith(side);
+        super.setLength(side);
+        super.setColor(color);
+        super.setFilled(filled);
+    }
+    public double getSide() {
+        return getWith();
+    }
+    public void setSide(double side) {
+
+    }
+    public void setWith(double side) {
+
+    }
+    public void setLength(double side) {
+
+    }
+    public String toString() {
+        return super.toString();
+    }
+
 
     
 }
---
+++ src/Time/Time.java
@@ -0,0 +1,7 @@
+package Time;
+
+/**
+ * Created by Darek on 2017-08-22.
+ */
+public class Time {
+}
