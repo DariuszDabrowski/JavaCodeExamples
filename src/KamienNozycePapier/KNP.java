9fe42700181220174051ada80955afaa16059db9
DariuszDabrowski <darkos81@wp.pl>
2017-08-25 00:25:04 +0200

    poprawki

--- src/KamienNozycePapier/KNP.java
+++ src/KamienNozycePapier/KNP.java
@@ -3,75 +3,98 @@ package KamienNozycePapier;
 import java.util.Random;
 import java.util.Scanner;
 
-import static KamienNozycePapier.KNP.dzialanie.n;
-
 public class KNP {
-    public enum dzialanie{
-        k, n, p, q;
-    }
+
+    public enum GameActiones{KAMIEN, NOZYCE, PAPIER, Q;}
 
     public static void main(String[] args) {
-
+        Scanner scaner = new Scanner(System.in);
+        Random rand = new Random();
 
         int wygrane = 0;
         int przegrane = 0;
         int remisy = 0;
+        int ileGier = 0;
 
-        while (true){
-            Random rand = new Random();
-            String[] tab = {"k", "n", "p"};
-            int liczba = rand.nextInt()%3;
-            String wylosowano = tab[liczba];
-            Scanner scaner = new Scanner(System.in);
-            String wybrano = scaner.nextLine();
-            switch (wybrano){
+        GameActiones playerMove = GameActiones.KAMIEN;
+        GameActiones cpuMove = GameActiones.NOZYCE;
+
+        System.out.println("Gra Kamien / Nozyce / Papier");
+
+        while (!playerMove.equals(GameActiones.Q)){
+            System.out.print("Twoj ruch: ");
+            int random = rand.nextInt(3);
+            switch (random){
+                case 0:
+                    cpuMove = GameActiones.KAMIEN;
+                    break;
+                case 1:
+                    cpuMove = GameActiones.NOZYCE;
+                    break;
+                case 2:
+                    cpuMove = GameActiones.PAPIER;
+                    break;
+            }
+
+            String decision = scaner.nextLine();
+
+            switch (decision){
                 case "k":
-                    if(wylosowano.equals("n")){
-                        System.out.println("Wygrana");
+                    if(cpuMove.equals(GameActiones.NOZYCE)){
+                        System.out.println("Komputer Wybrał NOŻYCE");
+                        System.out.println("WYGRALES");
                         wygrane++;
-                    } else if (wylosowano.equals("p")){
-                        System.out.println("Przegrana");
+                    } else if (cpuMove.equals(GameActiones.PAPIER)){
+                        System.out.println("Komputer Wybrał PAPIER");
+                        System.out.println("PRZEGRALES");
                         przegrane++;
-                    } else if (wylosowano.equals("k")) {
-                        System.out.println("Remis");
+                    } else if (cpuMove.equals(GameActiones.KAMIEN)) {
+                        System.out.println("Komputer Wybrał KAMIEN");
+                        System.out.println("REMIS");
                         remisy++;
                     }
                     break;
                 case "n":
-                    if(wylosowano.equals("p")){
-                        System.out.println("Wygrana");
+                    if(cpuMove.equals(GameActiones.PAPIER)){
+                        System.out.println("Komputer Wybrał PAPIER");
+                        System.out.println("WYGRALES");
                         wygrane++;
-                    } else if (wylosowano.equals("k")){
-                        System.out.println("Przegrana");
+                    } else if (cpuMove.equals(GameActiones.KAMIEN)){
+                        System.out.println("Komputer Wybrał KAMIEN");
+                        System.out.println("PRZEGRALES");
                         przegrane++;
-                    } else if (wylosowano.equals("n")) {
-                        System.out.println("Remis");
+                    } else if (cpuMove.equals(GameActiones.NOZYCE)) {
+                        System.out.println("Komputer Wybrał NOZYCE");
+                        System.out.println("REMIS");
                         remisy++;
                     }
                     break;
                 case "p":
-                    if(wylosowano.equals("k")){
-                        System.out.println("Wygrana");
+                    if(cpuMove.equals(GameActiones.KAMIEN)){
+                        System.out.println("Komputer Wybrał KAMIEN");
+                        System.out.println("WYGRALES");
                         wygrane++;
-                    } else if (wylosowano.equals("n")){
-                        System.out.println("Przegrana");
+                    } else if (cpuMove.equals(GameActiones.NOZYCE)){
+                        System.out.println("Komputer Wybrał NOZYCE");
+                        System.out.println("PRZEGRALES");
                         przegrane++;
-                    } else if (wylosowano.equals("p")) {
-                        System.out.println("Remis");
+                    } else if (cpuMove.equals(GameActiones.PAPIER)) {
+                        System.out.println("Komputer Wybrał PAPIER");
+                        System.out.println("REMIS");
                         remisy++;
                     }
                     break;
                 case "q":
-                    System.out.printf("wygrane - %d, przegrane - %d, remisy - d%",wygrane, przegrane,remisy);
+                    playerMove = GameActiones.Q;
+                    System.out.printf("zagrales - %d razy\n " +
+                            "wygrales - %d razy\n przegrales - %d razy\n" +
+                            " zremisowales - %d razy",ileGier, wygrane, przegrane,remisy);
                     break;
                 default:
-                    System.out.println("Nie znana akcja!");
+                    continue;
             }
+            ileGier++;
         }
 
-
     }
-
-
-
 }
