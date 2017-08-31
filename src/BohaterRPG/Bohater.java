0d2748d5a87f761d493934d007730154b8891eaa
DariuszDabrowski <darkos81@wp.pl>
2017-08-31 13:36:20 +0200

    Bohater RPG

--- src/BohaterRPG/Bohater.java
+++ src/BohaterRPG/Bohater.java
@@ -1,8 +1,66 @@
 package BohaterRPG;
 
 public class Bohater {
-   protected String imie;
-   protected Tarcza tarcza;
-   protected Miecz miecz;
-   protected Plecak plecak;
+   protected String imie = "Bezimienny";
+   protected Tarcza tarcza = new DrewnianaTarcza();
+   protected Miecz miecz = new MieczKrotki();
+   protected Plecak plecak = new PlecakWluczegi();
+
+   public Bohater(){};
+
+   public Bohater(String imie){
+      setImie(imie);
+   }
+
+   public Bohater(String imie,Tarcza tarcza, Miecz miecz, Plecak plecak){
+      setImie(imie);
+      setTarcza(tarcza);
+      setMiecz(miecz);
+      setPlecak(plecak);
+   }
+
+   public String getImie() {
+      return imie;
+   }
+
+   public void setImie(String imie) {
+      this.imie = imie;
+   }
+
+   public Tarcza getTarcza() {
+      return tarcza;
+   }
+
+   public void setTarcza(Tarcza tarcza) {
+      this.tarcza = tarcza;
+   }
+
+   public Miecz getMiecz() {
+      return miecz;
+   }
+
+   public void setMiecz(Miecz miecz) {
+      this.miecz = miecz;
+   }
+
+   public Plecak getPlecak() {
+      return plecak;
+   }
+
+   public void setPlecak(Plecak plecak) {
+      this.plecak = plecak;
+   }
+
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("\n Imie: " + imie + "\n")
+              .append("Obrona: " + tarcza.obrona() + "\n")
+              .append("Atak: " + miecz.atak() + "\n")
+              .append("Posiadana Broń: \n")
+              .append("Tarcza: " + tarcza.pobierzOpis() + "\n")
+              .append("Miecz: " + miecz.pobierzOpis() + "\n")
+              .append("Plecak: " + plecak.pobierzOpis())
+              .append(" ( " + plecak.pojemnosc() + " )");
+      return sb.toString();
+   }
 }
--- src/BohaterRPG/Main.java
+++ src/BohaterRPG/Main.java
@@ -3,10 +3,32 @@ package BohaterRPG;
 public class Main {
 
     public static void main(String[] args) {
-        MieczDwureczny mdw = new MieczDwureczny();
-        MagicznaRuda mr = new MagicznaRuda(mdw);
+
+        Bohater bezimienny = new Bohater();
+        System.out.println(bezimienny);
+
+        ZelaznaTarcza zt = new ZelaznaTarcza();
+        SkoraTrola st = new SkoraTrola(zt);
+        SmoczaLuska sl = new SmoczaLuska(st);
+
+        MieczDwureczny md = new MieczDwureczny();
+        MagicznaRuda mr = new MagicznaRuda(md);
         LzyInnosa li = new LzyInnosa(mr);
-        System.out.printf("Nazwa: %s -> %d ataku."
-                , li.pobierzOpis(), li.atak());
+
+        PlecakWojownika pw = new PlecakWojownika();
+        ZloteNici zn = new ZloteNici(pw);
+
+        Bohater gorn = new Bohater();
+        gorn.setImie("Gorn");
+        gorn.setTarcza(sl);
+        gorn.setMiecz(li);
+        gorn.setPlecak(zn);
+        System.out.println(gorn);
+
+//        MieczDwureczny mdw = new MieczDwureczny();
+//        MagicznaRuda mr = new MagicznaRuda(mdw);
+//        LzyInnosa li = new LzyInnosa(mr);
+//        System.out.printf("Nazwa: %s -> %d ataku."
+//                , li.pobierzOpis(), li.atak());
     }
 }
